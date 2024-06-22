dd3de2c913085e563f70145a2a0743e870c15f55
Igor Nemykin <webroru@gmail.com>
2024-06-22 16:47:15 +0200

    update settings.php

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -132,3 +132,10 @@ $databases['d7']['default'] = [
 $settings['config_sync_directory'] = '../config/sync';
 
 $settings['file_private_path'] = getenv('FILE_PRIVATE_PATH', '../private');
+
+$settings['trusted_host_patterns'] = [
+    '^localhost$',
+    '^ntb.donstu.ru$',
+];
+
+$settings['rebuild_access'] = false;
